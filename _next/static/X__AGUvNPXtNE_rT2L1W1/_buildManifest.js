dfe9b7e07c7a7171c0b6965e2112565c18ffbcfc
MarkkuPekkarinen <markku.pekkarinen@gmail.com>
2026-06-19 23:34:53 +0000

    Update dashboard-artifact to output generated at a10c7cd

---
+++ _next/static/X__AGUvNPXtNE_rT2L1W1/_buildManifest.js
@@ -0,0 +1,90 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0iz8gqf5fu0d_.js"
+  ],
+  "/_error": [
+    "static/chunks/0dk_crv70-idb.js"
+  ],
+  "/await_tree": [
+    "static/chunks/0e_sc22en6wdv.js"
+  ],
+  "/batch_tasks": [
+    "static/chunks/10bdqq-vop5xz.js"
+  ],
+  "/cluster": [
+    "static/chunks/0i7c7y13lhx4u.js"
+  ],
+  "/explain_distsql": [
+    "static/chunks/0c7fncmnnnh2p.js"
+  ],
+  "/fragment_graph": [
+    "static/chunks/0i78if3ig0slx.js"
+  ],
+  "/functions": [
+    "static/chunks/10yi_n~foj5m9.js"
+  ],
+  "/heap_profiling": [
+    "static/chunks/0g8nxkzo~rde7.js"
+  ],
+  "/indexes": [
+    "static/chunks/11qqzjgez-19p.js"
+  ],
+  "/internal_tables": [
+    "static/chunks/0ke-_aos8azbd.js"
+  ],
+  "/materialized_views": [
+    "static/chunks/0w.ybxux.~957.js"
+  ],
+  "/node": [
+    "static/chunks/0rgkgjyrmwz9z.js"
+  ],
+  "/relation_graph": [
+    "static/chunks/0121.c75a4ftm.js"
+  ],
+  "/settings": [
+    "static/chunks/18amoa7z9h9qw.js"
+  ],
+  "/sinks": [
+    "static/chunks/01927nq6fzboo.js"
+  ],
+  "/sources": [
+    "static/chunks/0yhj4rdul-.v-.js"
+  ],
+  "/subscriptions": [
+    "static/chunks/0h4k~wk.j_7us.js"
+  ],
+  "/tables": [
+    "static/chunks/10r7x1w4p-9gf.js"
+  ],
+  "/views": [
+    "static/chunks/0u03_a5x6wrfy.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/await_tree",
+    "/batch_tasks",
+    "/cluster",
+    "/explain_distsql",
+    "/fragment_graph",
+    "/functions",
+    "/heap_profiling",
+    "/indexes",
+    "/internal_tables",
+    "/materialized_views",
+    "/node",
+    "/relation_graph",
+    "/settings",
+    "/sinks",
+    "/sources",
+    "/subscriptions",
+    "/tables",
+    "/views"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
